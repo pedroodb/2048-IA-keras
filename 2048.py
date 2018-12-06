0a60165ede99a93d5bc05933f351c922edb054c0
Pedro Dal Bianco <dalbianco.pedro@gmail.com>
2018-12-06 12:14:02 -0300

    Init

--- 2048.py
+++ 2048.py
@@ -1,13 +1,22 @@
-import random
 import math
-
+import random
 
 class Matrix:
 
-    matrix = [[0 for x in range(4)] for y in range(4)]
-
     def __init__(self):
+        self.matrix = [[0 for _ in range(4)] for _ in range(4)]
+        self.score = 0
         self.add_two()
+        
+    def reset(self):
+        self.__init__()
+    
+    def status(self):
+        return [i for line in self.matrix for i in line ]
+
+    def step(self, move):
+        self.move(move)
+        return self.status(),self.score,self.add_two(),''
 
     def clone(self):
         new_matrix = Matrix()
@@ -53,7 +62,9 @@ class Matrix:
                             line[j] = 0
                         elif line[j+1] == line[j]:
                             if not joint:
-                                line[j + 1] = (line[j]*2)
+                                res = (line[j]*2)
+                                line[j + 1] = res
+                                self.score += res
                                 line[j] = 0
                                 joint = True
 
@@ -73,65 +84,6 @@ class Matrix:
             return True
 
 
-class Game:
-
-    matrix = Matrix()
-
-    @staticmethod
-    def save_to_file(matrix, movement):
-        with open("matrix_history.txt", "a") as matrix_history:
-            print(matrix, file=matrix_history)
-        with open("movement_history.txt", "a") as movement_history:
-            print(movement, file=movement_history)
-
-    @staticmethod
-    def get_direction(move):
-        # Get a movement direction from w,a,s or d
-        return {
-            'd': 0,
-            'w': 1,
-            'a': 2,
-            's': 3,
-        }.get(move, -1)
-
-    def move(self, move):
-        direction = self.get_direction(move)
-        if direction != -1:
-            return self.matrix.move(direction)
-        else:
-            print('Invalid movement, try with w,a,s or d (hola soy Juan)'
-                  '')
-
-    def print_matrix(self):
-        self.matrix.print()
-
-    def play(self):
-        playing = True
-        while playing:
-            self.print_matrix()
-            movement = input()
-            f = Formater()
-            if self.get_direction(movement) != -1:
-                self.save_to_file(f.format(self.matrix.matrix), self.get_direction(movement))
-            if self.move(movement):
-                playing = self.matrix.add_two()
-        print('You lost')
-
-
-class Formater:
-
-    def log(self, num):
-        if num != 0:
-            return int(math.log(num, 2))
-        else:
-            return 0
-
-    def format(self, matrix):
-        formated_str = ""
-        for line in matrix:
-            for i in line:
-                formated_str += str(self.log(i))
-        return formated_str
-
-g = Game()
-g.play()
+a = Matrix()
+while True:
+    print(a.step(int(input())))
